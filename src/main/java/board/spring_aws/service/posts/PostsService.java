e2a4ae314a167c6356978a08d6d5a9950a0d9002
kim-jin-bum <kgy5988@naver.com>
2024-07-27 19:08:14 +0900

    feat: 게시물 등록 기능 추가
    Request 데이터를 받을 Dto
    API 요청을 받을 Controller
    트랜잭션, 도메인 기능 간의 순서를 보장하는 Service

---
+++ src/main/java/board/spring_aws/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package board.spring_aws.service.posts;
+
+import board.spring_aws.domain.posts.PostsRepository;
+import board.spring_aws.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+
+}
