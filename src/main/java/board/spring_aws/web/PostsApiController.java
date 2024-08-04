7986a57b47dec2095864bbf331a320a0805b4bdb
kim-jin-bum <kgy5988@naver.com>
2024-08-04 23:57:02 +0900

    feat: 게시물 수정, 조회 기능 추가
    h2 데이터베이스에 더미 데이터 insert 후 API 테스트 완료

--- src/main/java/board/spring_aws/web/PostsApiController.java
+++ src/main/java/board/spring_aws/web/PostsApiController.java
@@ -1,11 +1,12 @@
 package board.spring_aws.web;
 
 import board.spring_aws.service.posts.PostsService;
+import board.spring_aws.web.dto.PostsResponseDto;
 import board.spring_aws.web.dto.PostsSaveRequestDto;
+import board.spring_aws.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,4 +18,21 @@ public class PostsApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
+
+    // URL 경로 변수나 쿼리 매개변수를 사용하여 메서드 파라미터를 명시적으로 지정하거나 -parameters 플래그를 설정하여 컴파일러가 메서드 파라미터 이름 정보를 포함하도록 한다.
+    // @PathVariable을 사용하여 URL 경로에서 ID를 추출하고, @RequestBody를 사용하여 요청 본문에서 DTO를 추출
+    @PutMapping("/api/v1/posts/{id}")
+    public ResponseEntity<Long> updatePost(
+            @PathVariable("id") Long id,
+            @RequestBody PostsUpdateRequestDto requestDto) {
+        Long updatedId = postsService.update(id, requestDto);
+        return ResponseEntity.ok(updatedId);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+
 }
---
+++ src/main/java/board/spring_aws/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package board.spring_aws.web.dto;
+
+import board.spring_aws.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
