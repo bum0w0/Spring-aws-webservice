1f049c8d422f94f663d3da3c86e5a4494baf3839
kim-jin-bum <kgy5988@naver.com>
2024-08-05 22:02:41 +0900

    feat: 기본적인 홈페이지, 게시글 등록 화면 추가
    템플릿 엔진은 thymeleaf 사용
    
    공통 영역(footer.html, header.html)을 별도의 파일로 분리하지 않고 필요한 곳(hello.html)에서 가져다 쓰는 레이아웃 방식 적용

--- src/main/java/board/spring_aws/web/HomeController.java
+++ src/main/java/board/spring_aws/web/HomeController.java
@@ -1,4 +1,4 @@
-package board.spring_aws;
+package board.spring_aws.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,5 +9,10 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "hello";  // resources/templates/index.html을 반환합니다.
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
