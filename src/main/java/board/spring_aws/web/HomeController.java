8b509907008cfc3a556387ba969c3188be03e092
kim-jin-bum <kgy5988@naver.com>
2024-08-05 22:59:00 +0900

    design: 게시글 수정, 삭제 화면 추가

--- src/main/java/board/spring_aws/web/HomeController.java
+++ src/main/java/board/spring_aws/web/HomeController.java
@@ -1,10 +1,12 @@
 package board.spring_aws.web;
 
 import board.spring_aws.service.posts.PostsService;
+import board.spring_aws.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +23,13 @@ public class HomeController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
